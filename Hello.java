9643e1ba38124635613fbb2ff46854048318ecb2
skhangare777 <skhangare777@gmail.com>
2019-08-25 00:12:26 +0530

    Updating Hello file

--- Hello.java
+++ Hello.java
@@ -5,6 +5,8 @@ class hello
     System.out.println("Hello World");
 
     System.out.println("hi friends! Do u wanna hangout with me.");
+    
+    System.out.println("Garden is the best place for hangout");
   }
 }
 
